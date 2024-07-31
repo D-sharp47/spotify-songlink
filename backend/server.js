dccb02532aee73b893c6974817b677115be0ef90
Denali Sharp <denalisharp@Denalis-MacBook-Pro.local>
2024-07-31 10:28:13 -0400

    started adding s3 for images

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ mongoose
   .catch((err) => console.log(err));
 
 // Middleware
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: false }));
 
 app.use(
